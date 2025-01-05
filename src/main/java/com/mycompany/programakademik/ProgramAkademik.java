cf89189a01ddc23ed6825a528372902c11309e50
Ardo Rianda <ardorianda1809@gmail.com>
2025-01-05 15:43:37 +0700

    Add files via upload

---
+++ src/main/java/com/mycompany/programakademik/ProgramAkademik.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.programakademik;
+
+/**
+ *
+ * @author ACER
+ */
+public class ProgramAkademik {
+
+    public static void main(String[] args) {
+        new menu().show();
+    }
+}
